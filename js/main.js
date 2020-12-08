736ffc1b5af5ba1e2b7e0a4ae690c510228761a5
caos2004 <oscar.ruizruiz@gmail.com>
2020-12-07 22:58:54 -0600

    master original

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
 console.log('Hi Koders')
 
 //alert('Mensaje de alerta')
-
-let name = prompt('¿Cuál es tu nombre?')
-
-console.log(name)
-
-let age_koder = prompt('¿Cuál es tu edad?')
-
-console.log(age_koder)
-
-let resultSuma = parseInt(age_koder)  + 5
-
-console.log('suma: ',  resultSuma)
-console.log('resta: ', age_koder - 5)
-console.log('mult: ',  age_koder * 5)
-console.log('div: ',   age_koder / 5)
