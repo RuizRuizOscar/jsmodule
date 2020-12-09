3ce57dc93b14f58a3ca17d8e1c49229124762470
caos2004 <oscar.ruizruiz@gmail.com>
2020-12-08 21:28:45 -0600

    ADD weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,23 +1,14 @@
-let weather = prompt('¿Cómo está el clima?')
+const WEATHER = prompt('¿Cómo está el clima?')
 
-if(weather=='soleado')
-{
-    console.log('El dia está '+weather)
-}
-else if(weather=='lluvioso')
-{
-    console.log('El dia está '+weather)
-}
-else if(weather=='nevando')
-{
-    console.log('El dia está '+weather)
-}
-else if(weather=='nublado')
-{
-    console.log('El dia está '+weather)
-}
-else 
-{
-    let weather_new = prompt('Por favor dame una descripción completa del clima en tu zona')
-    console.log(`${weather_new}`)
+if(WEATHER=='soleado'){
+    console.log('Qué buen día para tender la ropa. Hoy sí se seca')
+}else if(WEATHER=='lluvioso'){
+    console.log('Hoy se antoja ver llover desde dentro con una taza de chocolate en mano')
+}else if(WEATHER=='nevando'){
+    console.log('¿Quién quiere hacer angelitos de nieve? ¡Hoy es el día!')
+}else if(WEATHER=='nublado'){
+    console.log('Un excelente día para salir a correr y no broncearse, pues las nubes te protegerán')
+}else {
+    const WEATHER_NEW = prompt('Por favor dame una descripción completa del clima en tu zona')
+    console.log(`${WEATHER_NEW}`)
 }
