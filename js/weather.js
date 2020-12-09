1752da60f9978ecd63c85e3418f788876e901a8c
caos2004 <oscar.ruizruiz@gmail.com>
2020-12-08 21:45:36 -0600

    UPDATE weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,34 @@
+
+// if(WEATHER=='soleado'){
+    //     console.log('Qué buen día para tender la ropa. Hoy sí se seca')
+// }else if(WEATHER=='lluvioso'){
+    //     console.log('Hoy se antoja ver llover desde dentro con una taza de chocolate en mano')
+    // }else if(WEATHER=='nevando'){
+        //     console.log('¿Quién quiere hacer angelitos de nieve? ¡Hoy es el día!')
+        // }else if(WEATHER=='nublado'){
+//     console.log('Un excelente día para salir a correr y no broncearse, pues las nubes te protegerán')
+// }else {
+    //     const WEATHER_NEW = prompt('Por favor dame una descripción completa del clima en tu zona')
+    //     console.log(`${WEATHER_NEW}`)
+    // }
+    
 const WEATHER = prompt('¿Cómo está el clima?')
 
-if(WEATHER=='soleado'){
-    console.log('Qué buen día para tender la ropa. Hoy sí se seca')
-}else if(WEATHER=='lluvioso'){
-    console.log('Hoy se antoja ver llover desde dentro con una taza de chocolate en mano')
-}else if(WEATHER=='nevando'){
-    console.log('¿Quién quiere hacer angelitos de nieve? ¡Hoy es el día!')
-}else if(WEATHER=='nublado'){
-    console.log('Un excelente día para salir a correr y no broncearse, pues las nubes te protegerán')
-}else {
-    const WEATHER_NEW = prompt('Por favor dame una descripción completa del clima en tu zona')
-    console.log(`${WEATHER_NEW}`)
+switch(WEATHER){
+    case 'soleado':
+        console.log('Qué buen día para tender la ropa. Hoy sí se seca')
+        break
+    case 'lluvioso':
+        console.log('Hoy se antoja ver llover desde dentro con una taza de chocolate en mano')
+        break
+    case 'nevando':
+        console.log('¿Quién quiere hacer angelitos de nieve? ¡Hoy es el día!')
+        break
+    case 'nublado':
+        console.log('Un excelente día para salir a correr y no broncearse, pues las nubes te protegerán')
+        break
+    default:    
+        const WEATHER_NEW = prompt('Por favor dame una descripción completa del clima en tu zona')
+        console.log(`${WEATHER_NEW}`)
 }
+    
