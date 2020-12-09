306ad2e7722f314459d4184867340c68713b91c8
caos2004 <oscar.ruizruiz@gmail.com>
2020-12-08 21:57:10 -0600

    UPDATE is_largest.js con ternario

--- js/is_largest.js
+++ js/is_largest.js
@@ -1,10 +1,19 @@
-console.log('is largest')
+// console.log('is largest')
 
-const NUM1 = parseInt(prompt('Dame un número: '))
-const NUM2 = parseInt(prompt('Dame otro número: '))
+// const NUM1 = parseInt(prompt('Dame un número: '))
+// const NUM2 = parseInt(prompt('Dame otro número: '))
 
-if (NUM1>NUM2){
-    console.log(`El mayor es: ${NUM1}`)
-}
-else
-    console.log(`El mayor es: ${NUM2}`)
+// if (NUM1>NUM2){
+//     console.log(`El mayor es: ${NUM1}`)
+// }
+// else
+//     console.log(`El mayor es: ${NUM2}`)
+
+
+const NUMBER_ONE = parseInt(prompt('Dame un número por favor: '))
+const NUMBER_TWO = parseInt(prompt('Dame otro número por favor: '))
+
+NUMBER_ONE > NUMBER_TWO ?
+    console.log(`El mayor es: ${NUMBER_ONE}`)
+    :
+    console.log(`El mayor es: ${NUMBER_TWO}`)
