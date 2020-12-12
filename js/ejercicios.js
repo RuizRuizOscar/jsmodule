29a68f1baa6ced75917364c3edcafe8381d787b2
caos2004 <oscar.ruizruiz@gmail.com>
2020-12-11 21:59:46 -0600

    ADD funciones printTablas, isPalindrome, getDataType

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,7 +1,24 @@
 function printTablas(tablas){
-    const number = tablas
-    let arrTablas
-    for(let i=0; i<parseInt(number); i++){
-        arrTablas
+    const num = tablas
+    for(let i=0; i<num.length; i++){
+        for(let j=0; j<=10;j++){
+            console.log(`${num[i]} x ${j} = ${ num[i] * j }`)
+        }
     }
+}
+
+function isPalindrome(phrase){
+    const pal_original = phrase
+    let pal_check = ''
+    let pal_no_space = ''
+    pal_no_space = pal_original.replace(/ /g, "");
+    for(let i=pal_no_space.length-1; i>=0 ; i--){
+        pal_check = pal_check.concat(pal_no_space[i])
+    }
+    if (pal_no_space === pal_check)
+        console.log(`Son iguales`);
+}
+
+function getDataType (par1, par2){
+    console.log(par1+' es de tipo '+ typeof par1 + ' y ' + par2 + ' es de tipo ' + typeof par2)
 }
